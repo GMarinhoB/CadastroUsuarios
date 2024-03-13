b575d4145c31792708f00acc299e1d9e807c4089
Marinho <contato.gmarinhobernardi@gmail.com>
2024-03-13 02:05:48 -0300

    Update formulario.sql

--- Cadastro/formulario.sql
+++ Cadastro/formulario.sql
@@ -1,1 +1,18 @@
+CREATE TABLE `formulario` (
+  `idformulario` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(25) NOT NULL,
+  `sobrenome` varchar(100) NOT NULL,
+  `cep` varchar(8) NOT NULL,
+  `rua` varchar(100) NOT NULL,
+  `cidade` varchar(45) NOT NULL,
+  `uf` varchar(2) NOT NULL,
+  `bairro` varchar(80) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `complemento` varchar(100) DEFAULT NULL,
+  `profissao` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idformulario`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+
+  
+
 SELECT * FROM formulario.formulario;
